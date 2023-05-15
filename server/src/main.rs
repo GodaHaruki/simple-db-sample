850666f7c5347420ed4be76bd91448add559ce42
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2023-05-15 06:39:44 +0000

    add db program

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,25 +1,42 @@
 use axum::extract::Path;
 use hyper::StatusCode;
-use serde_json::json;
-use simple_db::db::DB;
+use serde::{Deserialize};
+use serde_json::{json, from_str};
+use simple_db::db::{DB};
 use axum::{Router, Json, Extension};
 use axum::routing::{put, get};
 use axum::response::IntoResponse;
-use std::collections::HashMap;
 use std::net::SocketAddr;
-use std::sync::Mutex;
-use std::sync::Arc;
+use std::println;
 
-async fn handle_get(key: Path<String>) -> impl IntoResponse {
-    println!("accessed");
-    println!("{}", key.as_str());
-    (
-        StatusCode::OK,
-        Json(json!({"message":"response!"})),
-    )
+#[derive(Deserialize, Debug)]
+struct GetInfo{
+    key: String
 }
 
-async fn handle_put(Extension(db): Extension<Arc<Mutex<HashMap<String, String>>>>, Path(post_data): Path<String>) -> impl IntoResponse {
+async fn handle_get(Extension(db): Extension<DB>, Path(get_data): Path<GetInfo>) -> impl IntoResponse {
+    let key = get_data.key;
+    println!("{:?}", &key);
+    if let Some(v) = db.get(key){
+        return (
+            StatusCode::OK,
+            Json(v)
+        )
+    } else {
+        return (StatusCode::NOT_FOUND, Json("not found".to_string()))
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct PutInfo {
+    key: String,
+    value: String,
+}
+
+async fn handle_put(Extension(db): Extension<DB>, put_data: String) -> impl IntoResponse {
+    println!("{:?}", &put_data);
+    let info = put_data;
+    // db.set(info.key, info.value);
     StatusCode::CREATED
 }
 
@@ -29,8 +46,9 @@ async fn main() {
 
     let app = Router::new()
         .route("/api/:key", get(handle_get))
-        .route("/api", put(handle_put)
-        .layer(Extension(db.get_arc())));
+        .layer(Extension(db.clone()))
+        .route("/put", put(handle_put)
+        .layer(Extension(db.clone())));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
